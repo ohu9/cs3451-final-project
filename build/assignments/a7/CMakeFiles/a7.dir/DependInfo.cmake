bad3cbfb0b1b9a009a886b694d844688ea73f623
ohu9 <oliviah221@gmail.com>
2024-04-20 14:29:53 -0400

    a9 starter code

---
+++ build/assignments/a7/CMakeFiles/a7.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/oliviahu/3451/cs3451-final-project/ext/stb/StbImage.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/ext/stb/StbImage.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/ext/stb/StbImage.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/ext/tiny_gltf/TinyGltfLoader.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/ext/tiny_gltf/TinyGltfLoader.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/ext/tiny_gltf/TinyGltfLoader.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLBufferObjects.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLBufferObjects.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLBufferObjects.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLMarkerObjects.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLMarkerObjects.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLMarkerObjects.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLObject.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLObject.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLObject.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLShaderProgram.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLShaderProgram.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLShaderProgram.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLTexture.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLTexture.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLTexture.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLViewer.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLViewer.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLViewer.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/OpenGLWindow.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLWindow.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/OpenGLWindow.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/glad.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/glad.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/glad.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/src/mikktspace.cpp" "assignments/a7/CMakeFiles/a7.dir/__/__/src/mikktspace.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/__/__/src/mikktspace.cpp.o.d"
+  "/Users/oliviahu/3451/cs3451-final-project/assignments/a7/main.cpp" "assignments/a7/CMakeFiles/a7.dir/main.cpp.o" "gcc" "assignments/a7/CMakeFiles/a7.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
